57165106ead682f0ddfb968cdda015738abe3bc5
R. Tyler Ballance <tyler@monkeypox.org>
2009-01-19 03:18:56 -0800

    Add the tests directory, before I dive head first into some IMAP and other code, I should have some tests
    Going to use CUnit to familiarize myself with the c-client code, as well as some GLib funness
    
    Signed-off-by: R. Tyler Ballance <tyler@monkeypox.org>

---
+++ tests/config.c
@@ -0,0 +1,44 @@
+/*
+ * Testing for the config file handling code
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <CUnit/Basic.h>
+
+void test_Void() {
+	CU_ASSERT(true == true);
+}
+
+int main()
+{
+   CU_pSuite pSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if (CUE_SUCCESS != CU_initialize_registry())
+      return CU_get_error();
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite("Config_Suite", NULL, NULL);
+   if (NULL == pSuite) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
+   if ((NULL == CU_add_test(pSuite, "empty test", test_Void)) )
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* Run all tests using the CUnit Basic interface */
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   CU_cleanup_registry();
+   return CU_get_error();
+}
+
