a95759749bbc247a7f79109f0b366e23039dd4a9
R. Tyler Ballance <tyler@monkeypox.org>
2009-01-19 04:07:54 -0800

    Add sample configuration file
    Start writing some GLib key-value file parsing test code to get a handle on it.
    
    I think the general plan is to write a simple configuration utility, that will generate a .config/imuse/configuration file,
    then let imuse use that as the default means of reading settings for the connections
    
    Another option, instead of a configuration utility, would be to present the settings file as a "special" file in the imuse file-system
    
    Signed-off-by: R. Tyler Ballance <tyler@monkeypox.org>

--- tests/config.c
+++ tests/config.c
@@ -6,14 +6,37 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include <glib.h>
+#include <glib/gprintf.h>
+
 #include <CUnit/Basic.h>
 
 void test_Void() {
 	CU_ASSERT(true == true);
 }
 
-int main()
-{
+void test_BasicConfigSample() {
+	GKeyFile *file = g_key_file_new();
+	CU_ASSERT_PTR_NOT_NULL(file);
+
+	CU_ASSERT(TRUE == g_key_file_load_from_file(file, "config.sample.ini", G_KEY_FILE_NONE, NULL));
+
+	char **groups = g_key_file_get_groups(file, NULL);
+	CU_ASSERT_PTR_NOT_NULL(groups);
+
+	int i = 0;
+	char *group = groups[i];
+	while (group != NULL) {
+		group = groups[++i];
+	}
+	CU_ASSERT(i == 2);
+	CU_ASSERT_STRING_EQUAL(groups[0], "General");
+	CU_ASSERT_STRING_EQUAL(groups[1], "Account");
+	g_strfreev(groups);
+}
+
+
+int main() {
    CU_pSuite pSuite = NULL;
 
    /* initialize the CUnit test registry */
@@ -29,7 +52,10 @@ int main()
 
    /* add the tests to the suite */
    /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
-   if ((NULL == CU_add_test(pSuite, "empty test", test_Void)) )
+   if (
+		(NULL == CU_add_test(pSuite, "empty test", test_Void)) || 
+		(NULL == CU_add_test(pSuite, "test reading config.sample", test_BasicConfigSample))
+	  )
    {
       CU_cleanup_registry();
       return CU_get_error();
