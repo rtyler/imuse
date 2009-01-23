653ab7c2f880b1ef44f49ff0638634b71d1e66f0
R. Tyler Ballance <tyler@monkeypox.org>
2009-01-23 00:25:41 -0800

    Moving email sending functionality into muse_send.[c/h]
    Signed-off-by: R. Tyler Ballance <tyler@monkeypox.org>

---
+++ muse_send.c
@@ -0,0 +1,18 @@
+/*
+ * imuse -- A FUSE filesystem for exposing IMAP accounts as a locally browsable filesystem, neat!
+ *
+ * (c) 2009 - R. Tyler Ballance <tyler@monkeypox.org>
+ */
+
+#include <glib.h>
+#include <imap/c-client.h>
+
+#include "muse_send.h"
+
+struct muse_send_config *config_by_account(const char *accountname) {
+
+	return NULL;
+}
+
+
+
---
+++ muse_send.h
@@ -0,0 +1,12 @@
+/*
+ * imuse -- A FUSE filesystem for exposing IMAP accounts as a locally browsable filesystem, neat!
+ *
+ * (c) 2009 - R. Tyler Ballance <tyler@monkeypox.org>
+ */
+
+#ifndef __MUSE_SEND_H
+#define __MUSE_SEND_H
+
+
+
+#endif
